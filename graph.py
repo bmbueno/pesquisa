e3f5db505747b5c5fd159d4d7addcf8e823ba91a
Bruno Morais <bruno.morais@inf.ufrgs.br>
2020-07-01 23:33:35 -0300

    First commit

---
+++ graph.py
@@ -0,0 +1,110 @@
+import sqlite3
+import igraph
+
+color_dict = {"clt": "pink", "estudante": "red",
+              "servidor": "blue", "autônomo": "black", "desempregado": "yellow"}
+q4 = {'mascaras': [], 'luvas': [], 'alcoolGel': [],
+      'sabao': [], 'nenhumAcesso': [], 'nenhumInteresse': []}
+q6 = {'normal': [], 'ansioso': [], 'depressivo': [], 'preocupado': []}
+q7 = {'clt': [], 'autônomo': [], 'estudante': [],
+      'servidor': [], 'desempregado': []}
+checkBoxes = {'q4': q4, 'q6': q6, 'q7': q7}
+nV = 0
+nA = 0
+
+# dados uma lista de vertices e um atributo cria arestas com este atributo entre cada elemento da lista de vertices
+
+
+def ligaArestas(v, attr):
+    nA = 0
+    for i in range(1, len(v)):
+        g.add_edges([(v[i-1], v[i])])
+        g.es[nA][attr] = True
+        g.es[nA]['color'] = color_dict[attr]
+        nA = nA + 1
+
+# dados um atributo e o numero de uma questao cria uma lista de vertices de mesmo atributo (resposta) para dada questao
+def listaAtributosV(attr, quest):
+    if(g.vs[nV][attr]):
+        checkBoxes[quest][attr].append(nV)
+
+
+g = igraph.Graph(0)
+
+
+dbConec = sqlite3.connect("pesquisaMin.db")
+db = dbConec.cursor()
+
+db.execute("""SELECT * FROM entrevistado;""")
+
+entrevistados = db.fetchall()
+
+print(entrevistados[len(entrevistados)-1])
+
+
+for row in entrevistados:
+    # print(row[0])
+
+    g.add_vertices(1)
+    g.vs[nV]['idade'] = row[2]
+
+    g.vs[nV]['clt'] = 'CLT' in row[3]
+    g.vs[nV]['estudante'] = 'Estudante' in row[3]
+    g.vs[nV]['autônomo'] = 'Autônomo' in row[3]
+    g.vs[nV]['servidor'] = 'Servidor' in row[3]
+    g.vs[nV]['desempregado'] = 'Desempregado' in row[3]
+
+    g.vs[nV]['8'] = row[4]
+    g.vs[nV]['9'] = row[5]
+    # g.vs[nV]['essencial'] = 'Sim' in row[4]
+    # g.vs[nV]['perfilSocio'] = str(row[5])
+
+    for attr in checkBoxes['q7']:
+        listaAtributosV(attr, 'q7')
+
+    respostas = db.execute(
+        """SELECT * FROM resposta WHERE entrevistadoID=(?) ;""", [str(row[0])])
+
+    for resposta in respostas:
+        if(resposta[1] != 4 and resposta[1] != 6):
+            g.vs[nV][str(resposta[1])] = resposta[3]
+        elif(resposta[1] == 4):
+            g.vs[nV]['mascaras'] = 'Máscaras' in resposta[3]
+            g.vs[nV]['luvas'] = 'Luvas' in resposta[3]
+            g.vs[nV]['alcoolGel'] = 'gel' in resposta[3]
+            g.vs[nV]['sabao'] = 'Sabão' in resposta[3]
+            g.vs[nV]['nenhumAcesso'] = 'acesso' in resposta[3]
+            g.vs[nV]['nenhumInteresse'] = 'interesse' in resposta[3]
+
+            for attr in checkBoxes['q4']:
+                listaAtributosV(attr, 'q4')
+        else:
+            g.vs[nV]['normal'] = 'Normal' in resposta[3]
+            g.vs[nV]['ansioso'] = 'Ansioso' in resposta[3]
+            g.vs[nV]['depressivo'] = 'Depressivo' in resposta[3]
+            g.vs[nV]['preocupado'] = 'Preocupado' in resposta[3]
+
+            for attr in checkBoxes['q6']:
+                listaAtributosV(attr, 'q6')
+
+    nV = nV+1
+
+
+for attr in q7:
+    ligaArestas(q7[attr], attr)
+for attr in q4:
+    ligaArestas(q4[attr], attr)
+for attr in q6:
+    ligaArestas(q6[attr], attr)
+
+# g.vs["label"] = g.vs['9']
+# g.es["label"] = g.es["estudante"]
+
+print(checkBoxes['q6'])
+print(g.vs['mascaras'])
+
+layout = g.layout("circle")
+igraph.plot(g)
+# print(db.lastrowid)
+
+dbConec.close()
---
+++ tratamento dados.py
@@ -0,0 +1,52 @@
+import csv
+import sqlite3
+
+with open('Pesquisa1.csv') as file:
+    fileReader = csv.reader(file, delimiter=',')
+
+
+    dbConec = sqlite3.connect("pesquisaMin.db")
+    db = dbConec.cursor()
+    
+    # db.execute('CREATE TABLE perfilSocioEconomico( id INTEGER PRIMARY KEY AUTOINCREMENT, resposta VARCHAR(100) NOT NULL);')
+    # db.execute("""INSERT INTO perfilSocioEconomico (resposta) VALUES ('De R$ 522,50 a R$ 1045,00;');""")
+    # db.execute("""INSERT INTO perfilSocioEconomico (resposta) VALUES ('Ate R$ 522,50;');""")
+    # db.execute("""INSERT INTO perfilSocioEconomico (resposta) VALUES ('De R$ 1045,00 a R$ 3135,00;');""")
+    # db.execute("""INSERT INTO perfilSocioEconomico (resposta) VALUES ('Acima de R$ 3135,00;');""")
+  
+  
+    # db.execute('CREATE TABLE questao( id INTEGER PRIMARY KEY AUTOINCREMENT, descricao VARCHAR(100) );')
+    # db.execute('CREATE TABLE entrevistado(id INTEGER PRIMARY KEY AUTOINCREMENT, data VARCHAR(100) NOT NULL, idade INTEGER NOT NULL, ocupacao VARCHAR(100) NOT NULL, trabalhoEssencial VARCHAR(3) NOT NULL, perfilSocioEconomico VARCHAR(50) NOT NULL);')
+    # #db.execute('CREATE TABLE entrevistado(id INTEGER PRIMARY KEY AUTOINCREMENT,  data VARCHAR(100) NOT NULL, idade VARCHAR(2) NOT NULL, ocupacao VARCHAR(100) NOT NULL, trabalhoEssencial VARCHAR(3) NOT NULL, perfilSocioEconomicoID INTEGER, FOREIGN KEY (perfilSocioEconomicoID) REFERENCES perfilSocioEconomico(id));')
+    # db.execute('CREATE TABLE resposta(id INTEGER PRIMARY KEY AUTOINCREMENT, questaoID INTEGER,  entrevistadoID INTEGER, resposta VARCHAR(100) NOT NULL, FOREIGN KEY (questaoID) REFERENCES questao(id), FOREIGN KEY (entrevistadoID) REFERENCES entrevistado(id));')
+    
+    # db.execute("""INSERT INTO questao (descricao) VALUES ('Qual sua idade?');""")
+    # db.execute("""INSERT INTO questao (descricao) VALUES ('Qual o seu meio principal de informação?');""")
+    # db.execute("""INSERT INTO questao (descricao) VALUES ('Como está sua rotina? ');""")
+    # db.execute("""INSERT INTO questao (descricao) VALUES ('Tem usado quais utensílios de prevenção indicados contra o coronavírus?');""")
+    # db.execute("""INSERT INTO questao (descricao) VALUES ('É a favor do isolamento social para contenção do vírus ? ');""")
+    # db.execute("""INSERT INTO questao (descricao) VALUES ('Como você está se sentindo neste momento de pandemia?');""")
+    # db.execute("""INSERT INTO questao (descricao) VALUES ('Qual(is) sua(s) ocupação(ões)? ');""")
+    # db.execute("""INSERT INTO questao (descricao) VALUES ('É/São em um serviço considerado essencial?');""")
+    # db.execute("""INSERT INTO questao (descricao) VALUES ('Qual sua renda mensal per capita antes da pandemia do Coronavírus?');""")
+    # db.execute("""INSERT INTO questao (descricao) VALUES ('Você teve a redução desta renda durante a pandemia? ');""")
+
+
+    dbConec.commit()
+    print( db.lastrowid)
+
+    next(fileReader)
+
+    for row in fileReader:
+        db.execute("""INSERT INTO entrevistado (data, idade, ocupacao, trabalhoEssencial, perfilSocioEconomico) VALUES (?,?,?,?,?);""", (row[0], row[1], row[7], row[8], row[9]))
+        idEntrevistado = db.lastrowid
+        db.execute("""INSERT INTO resposta (questaoID, entrevistadoID, resposta) VALUES (?,?,?);""", (2, idEntrevistado, row[2]))
+        db.execute("""INSERT INTO resposta (questaoID, entrevistadoID, resposta) VALUES (?,?,?);""", (3, idEntrevistado, row[3]))
+        db.execute("""INSERT INTO resposta (questaoID, entrevistadoID, resposta) VALUES (?,?,?);""", (4, idEntrevistado, row[4]))
+        db.execute("""INSERT INTO resposta (questaoID, entrevistadoID, resposta) VALUES (?,?,?);""", (5, idEntrevistado, row[5]))
+        db.execute("""INSERT INTO resposta (questaoID, entrevistadoID, resposta) VALUES (?,?,?);""", (6, idEntrevistado, row[6]))
+        db.execute("""INSERT INTO resposta (questaoID, entrevistadoID, resposta) VALUES (?,?,?);""", (10, idEntrevistado, row[10]))
+        dbConec.commit()
+        
+    dbConec.close()
+
