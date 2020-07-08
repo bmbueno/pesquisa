2e98c1022c226bc41f12ddb4110a09eb03f3e8e4
Bruno Morais <bruno.morais@inf.ufrgs.br>
2020-07-08 14:09:28 -0300

    Realizacao de testes de casos e adicao das imagens dos resultados

--- graph.py
+++ graph.py
@@ -8,6 +8,13 @@ q4 = {'mascaras': [], 'luvas': [], 'alcoolGel': [],
 q6 = {'normal': [], 'ansioso': [], 'depressivo': [], 'preocupado': []}
 q7 = {'clt': [], 'autônomo': [], 'estudante': [],
       'servidor': [], 'desempregado': []}
+
+q2Colors = {'Redes sociais, amigos, familiares, grupos de whats.': 'red', 'TV, sites oficiais de notícia, rádios': 'blue', 'Trabalho/estudo na área da saúde': 'green'}
+q5Colors = {'Sim': 'pink', 'Não': 'orange'}
+q10Colors = {'Sim, muito (mais da metade)': 'red', 'Sim, pouco (menos da metade)': 'green', 'Não': 'blue'}
+q6Colors = {'Normal': 'pink', 'Ansioso': 'black', 'Depressivo': 'yellow', 'Preocupado': 'orange'}
+q3Colors = {'Normal, saindo para estudo/trabalho presencial': 'green', 'Saindo para outras atividades': 'red', 'Saindo só quando é inevitável': 'blue', 'Em isolamento total':'magenta'}
+
 checkBoxes = {'q4': q4, 'q6': q6, 'q7': q7}
 nV = 0
 nA = 0
@@ -91,23 +98,47 @@ for row in entrevistados:
 # Para criar arestas no grafo com respostas de questões com multiplas respostas basta utilizar g.vs.select(atributo=True)
 # para cada opção da resposta, no caso já existem dicionarios definidos com estas opções e já previamente preenchidos com
 # uma lista de vértices nos quais as respostas são true, basta liga-los utilizando a funcao connectVertices, exemplo:
-for attr in q7:
-    connectVertices(q7[attr], color_dict[attr])
+# for attr in q7:
+#     connectVertices(q7[attr], color_dict[attr])
                     # o dicionario color_dict é alterável de acordo com a preferencia do usuario
 
 # Para criar arestas com respostas das demais questoes deve-se utilizar a funcao pronta do igraph "select" pegar os indices
 # dos vertices que correspondem a resposta requerida e utilizar novamente a funcao connectVertives, exemplo:
 
-a = g.vs.select(Q2='Trabalho/estudo na área da saúde')
-connectVertices(a.indices, 'orange')
+# 
+# for attr in q3Colors:
+#     a = g.vs.select(Q3=attr)
+#     connectVertices(a.indices, q3Colors[attr])
+
+
+# connectVertices(q6['normal'], q6Colors['Normal'])
+# connectVertices(q6['ansioso'], q6Colors['Ansioso'])
+# connectVertices(q6['preocupado'], q6Colors['Preocupado'])
+# connectVertices(q6['depressivo'], q6Colors['Depressivo'])
+
+# Rotina x com o sentimento (bloco de codigo acima)
+# for attr in q10Colors:
+#     a = g.vs.select(Q10=attr)
+#     connectVertices(a.indices, q10Colors[attr])
+
+# Meios de informação e favor ou nao ao isolamento
+for attr in q2Colors:
+    a = g.vs.select(Q2=attr)
+    connectVertices(a.indices, q2Colors[attr])
+
+for attr in q5Colors:
+    a = g.vs.select(Q5=attr)
+    connectVertices(a.indices, q5Colors[attr])
+
 
 layout = g.layout("circle")
-igraph.plot(g, layout=layout, vertex_size=10)
+igraph.plot(g, vertex_size=10)
 
-# print('Diametro do grafo: ' + str(g.diameter()))
-# print('Densidade do grafo: ' + str(g.density()))
+print('Diametro do grafo: ' + str(g.diameter()))
+print('Densidade do grafo: ' + str(g.density()))
 
-# print('Grau maximo do grafo: ' + str(max(g.degree())))
-# print('Grau minimo do grafo: ' + str(min(g.degree())))
+print('Grau maximo do grafo: ' + str(max(g.degree())))
+print('Grau minimo do grafo: ' + str(min(g.degree())))
+print(g.transitivity_undirected())
 
 dbConec.close()
